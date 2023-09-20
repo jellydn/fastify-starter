eeb17969ea85d00a5281bc5b5059411fc5035205
Huynh Duc Dung <dunghd.it@gmail.com>
2023-09-21 00:15:59 +0800

    :recycle: Fix fastify server typecheck error

--- src/server.ts
+++ src/server.ts
@@ -7,51 +7,55 @@ import Fastify from "fastify";
 import { initGraphql } from "./graphql";
 import { initSwagger } from "./swagger";
 
+import app from "./app";
+
 // Read the .env file.
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
 // Instantiate Fastify with some config
-const app = Fastify({
+const server = Fastify({
   logger: !isProduction,
 });
 
 // Register your application as a normal plugin.
-void app.register(import("./app"));
+void server.register(app);
 
 // Init graphql
-void initGraphql(app);
+void initGraphql(server);
 
 // Init Swagger
-void initSwagger(app);
+void initSwagger(server);
 
 // Delay is the number of milliseconds for the graceful close to finish
 const closeListeners = closeWithGrace({ delay: 500 }, async (opts: any) => {
   if (opts.err) {
-    app.log.error(opts.err);
+    server.log.error(opts.err);
   }
 
-  await app.close();
+  await server.close();
 });
 
-app.addHook("onClose", async (_instance, done) => {
+server.addHook("onClose", (_instance, done) => {
   closeListeners.uninstall();
   done();
 });
 
 // Start listening.
-void app.listen({
+void server.listen({
   port: Number(process.env.PORT ?? 3000),
   host: process.env.SERVER_HOSTNAME ?? "127.0.0.1",
 });
 
-app.ready((err: Error) => {
+server.ready((err: Error) => {
   if (err) {
-    app.log.error(err);
+    server.log.error(err);
     process.exit(1);
   }
 
-  app.log.info(`Server listening on port ${Number(process.env.PORT ?? 3000)}`);
+  server.log.info(
+    `Server listening on port ${Number(process.env.PORT ?? 3000)}`,
+  );
 });
 
-export { app };
+export { server as app };
