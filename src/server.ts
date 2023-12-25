6de2d39128369104e69e99e5ea2ce68a58eec483
Huynh Duc Dung <dunghd.it@gmail.com>
2023-12-26 00:57:36 +0800

    chore: fix linter

--- src/server.ts
+++ src/server.ts
@@ -47,7 +47,7 @@ void server.listen({
   host: process.env.SERVER_HOSTNAME ?? "127.0.0.1",
 });
 
-server.ready((err: Error) => {
+void server.ready((err) => {
   if (err) {
     server.log.error(err);
     process.exit(1);
